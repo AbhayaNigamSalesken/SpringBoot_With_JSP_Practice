2531fbd7f6bbea67c638a8345a42c1014fe7f3c6
AbhayaNigamSalesken <abhaya@salesken.ai>
2022-11-24 20:06:54 +0530

    userdetails jsp

--- userdetails/userdetails/src/main/java/UserdetailsApplication.java
+++ userdetails/userdetails/src/main/java/UserdetailsApplication.java
@@ -1,11 +1,12 @@
-package com.example.userdetails;
-
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = "com.example.userdetails")
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class UserdetailsApplication extends SpringBootServletInitializer {
 
 	@Override
